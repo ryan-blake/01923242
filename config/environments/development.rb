6521882241e238d0b5f4ccbeee21d5b1174afe60
Ryan Kliemann <Ryan_blake@icloud.com>
2016-08-22 14:32:48 -0500

    update prod/dev mailing

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
   # mailer
-  config.action_mailer.default_url_options = { host: '127.0.0.1', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost' }
   config.action_mailer.perform_deliveries = true
 
 
